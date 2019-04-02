dea70be08c6271a25ea23008958360fb8092cecd
Grzesiek Matejko <grzesiek.matejko@gmail.com>
2019-04-02 22:07:31 +0200

    .

--- config/intervalFunctions.js
+++ config/intervalFunctions.js
@@ -30,45 +30,49 @@ exports.checkBookingExpired = function(db){
     setInterval(() => {
         test(db);
     }, 5000);
-    //setInterval(intervalFunc, 5000);
   }
 function test(db){
     console.log(db);
 }
-function intervalFunc() {
+function intervalFunc(db) {
     console.log('start interval function');
-    //retrive all users -> may be need to send push notification
-    User.find({ accepted: true , bookings: { $gt: 0 } })
-        .then(users => {
-            //find all bookings that are not closed yet
-            Booking.find({closed: false} )
-                .then(bookings => {
-                    //check if any booking should be closed 
-                    bookings.array.forEach(booking => {
+    db.getInstance(function (p_db) {
+        User = p_db.collection('users');
+        Booking = p_db.collection('bookings'); 
+        
+        //retrive all users -> may be need to send push notification
+        User.find({ accepted: true , bookings: { $gt: 0 } })
+            .then(users => {
+                //find all bookings that are not closed yet
+                Booking.find({closed: false} )
+                    .then(bookings => {
+                        //check if any booking should be closed 
+                        bookings.array.forEach(booking => {
 
-                        var date = moment(booking.date + ' ' + booking.endTime, 'DD-MM-YYYY HH.mm');
-                        var tommorow = moment(date.add('1', 'days').format('DD-MM-YYYY'), 'DD-MM-YYYY');
-                        var diff = tommorow.diff(moment(), 'days');
-                        if (diff < 0 && !booking.closed) {
-                            //update booking 
-                            Booking.findOneAndUpdate({_id: booking._id}, {$set: {closed: true}})
-                                .then(x => {
-                                    var user = users.find(user => user._id == booking.user);
-                                    if(user){
-                                        bookingClosedNotification(user.devices[user.devices.length -1]);
-                                    }
-                                })
-                                .catch(err =>{
+                            var date = moment(booking.date + ' ' + booking.endTime, 'DD-MM-YYYY HH.mm');
+                            var tommorow = moment(date.add('1', 'days').format('DD-MM-YYYY'), 'DD-MM-YYYY');
+                            var diff = tommorow.diff(moment(), 'days');
+                            if (diff < 0 && !booking.closed) {
+                                //update booking 
+                                Booking.findOneAndUpdate({_id: booking._id}, {$set: {closed: true}})
+                                    .then(x => {
+                                        var user = users.find(user => user._id == booking.user);
+                                        if(user){
+                                            bookingClosedNotification(user.devices[user.devices.length -1]);
+                                        }
+                                    })
+                                    .catch(err =>{
 
-                                });
-                        }
+                                    });
+                            }
+                        });
+                    })
+                    .catch(err =>{
+                        console.log(err);
                     });
-                })
-                .catch(err =>{
-                    console.log(err);
-                });
-        })
-        .catch(err =>{
-            console.log(err);
-        });
+            })
+            .catch(err =>{
+                console.log(err);
+            });
+    });
 }
