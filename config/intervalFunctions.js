fa838bd1b767bf99f2de4146377fd191fd6860aa
Grzesiek Matejko <grzesiek.matejko@gmail.com>
2019-04-02 21:21:18 +0200

    booking interval

--- config/intervalFunctions.js
+++ config/intervalFunctions.js
@@ -6,12 +6,12 @@ var apnProvider = new apn.Provider({
   production: false,
 });
 
-async function userAcceptNotification(deviceId, user) {
+async function bookingClosedNotification(deviceId) {
     var note = new apn.Notification();
     note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
     note.badge = 1;
-    note.alert = `\uD83D\uDCE7 \u2709 Your account has beed accepted!`;
-    note.payload = { message: `Now you have access to all resources`, pushType: 'userAccept' };
+    note.alert = `\uD83D\uDCE7 \u2709 Booking closed!`;
+    note.payload = { message: `Your booking has been closed you lost change to get points`, pushType: 'bookingClosed' };
   
     var data = await apnProvider.send(note, deviceId);
     return data;
@@ -28,9 +28,43 @@ async function userAcceptNotification(deviceId, user) {
 
 
 exports.checkBookingExpired = function(){
-    setInterval(intervalFunc,1500);
+    setInterval(intervalFunc, 5000);
   }
-  
+
 function intervalFunc() {
-    console.log("It is working");
+    console.log('start interval function');
+    //retrive all users -> may be need to send push notification
+    User.find( {  accepted: true , bookings: { $gt: 0 } })
+        .then(users => {
+            //find all bookings that are not closed yet
+            Booking.find({closed: false} )
+                .then(bookings => {
+                    //check if any booking should be closed 
+                    bookings.array.forEach(booking => {
+
+                        var date = moment(booking.date + ' ' + booking.endTime, 'DD-MM-YYYY HH.mm');
+                        var tommorow = moment(date.add('1', 'days').format('DD-MM-YYYY'), 'DD-MM-YYYY');
+                        var diff = tommorow.diff(moment(), 'days');
+                        if (diff < 0 && !booking.closed) {
+                            //update booking 
+                            Booking.findOneAndUpdate({_id: booking._id}, {$set: {closed: true}})
+                                .then(x => {
+                                    var user = users.find(user => user._id == booking.user);
+                                    if(user){
+                                        bookingClosedNotification(user.devices[user.devices.length -1]);
+                                    }
+                                })
+                                .catch(err =>{
+
+                                });
+                        }
+                    });
+                })
+                .catch(err =>{
+                    console.log(err);
+                });
+        })
+        .catch(err =>{
+            console.log(err);
+        });
 }
