2bb847cec070ab5f06717ef7ef1bf9f7f9b017a5
Kamil Kozik <36657834+kozikkam@users.noreply.github.com>
2019-10-03 18:16:38 +0200

    Update migrations (#184)

--- migrations/11-09-19.js
+++ migrations/11-09-19.js
@@ -5,25 +5,33 @@ module.exports = (app, User, Place, Offer, Counter, Booking, OfferPost, Interval
   app.get('/api/migrate', async (req, res) => {
     console.log('1');
     await Offer.updateMany({}, { $set: { isActive: true } });
+    console.log('2');
     await Offer.updateMany({}, { $set: { scopes: ['regular'] } });
+    console.log('3');
 
     await Place.updateMany({ }, { $set: { allows: ['male', 'female']} });
+    console.log('4');
     await Place.updateMany({ }, { $set: { city: 'Milan' } });
+    console.log('5');
     await Place.updateMany({ }, { $set: { requirements: {} } });
-    console.log('2');
+    console.log('6');
 
     await Offer.updateMany({}, { $set: { isActive: true } });
+    console.log('7');
     await Offer.updateMany({}, { $set: { scopes: ['regular'] } });
+    console.log('8');
 
     let a, ps, users, places;
     a = await Booking.find({}).toArray();
+    console.log('9');
 
     ps = await Promise.all(a.map(async (el) => {
       await Booking.replaceOne({ _id: el._id }, { ...el, eventId: null });
     }));
-    console.log('3');
+    console.log('10');
     
     a = await Booking.find({}).toArray();
+    console.log('11');
 
     ps = await Promise.all(a.map(async (el) => {
       try {
@@ -32,49 +40,62 @@ module.exports = (app, User, Place, Offer, Counter, Booking, OfferPost, Interval
         await Booking.replaceOne({ _id: el._id }, el);
       } catch (err) {}
     }));
-    console.log('4');
+    console.log('12');
 
     users = await User.find({}).toArray();
+    console.log('13');
 
     await Promise.all(users.map(async (newUser) => {
       newUser.subscriptionPlan = { subscription: 'Unlimited' };
       await User.replaceOne({ _id: newUser._id }, newUser);
     }));
-    console.log('5');
+    console.log('14');
 
     places = await Place.find({}).toArray();
+    console.log('15');
 
     await Promise.all(places.map(async (place) => {
       place.access = 'basic';
       await Place.replaceOne({ _id: place._id }, place);
     }));
-    console.log('6');
+    console.log('16');
 
     await Booking.updateMany({date: '10-03-2019'}, {$set: {closed: false}});
-    console.log('7');
 
     return res.send('done');
   });
 
   app.get('/api/revert-migrate', async (req, res) => {
     await Offer.updateMany({}, { $unset: { isActive: '' } });
+    console.log('1');
     await Offer.updateMany({}, { $unset: { scopes: '' } });
+    console.log('2');
 
     await Place.updateMany({ }, { $unset: { allows: '' } });
+    console.log('3');
     await Place.updateMany({ }, { $unset: { city: '' } });
+    console.log('4');
     await Place.updateMany({ }, { $unset: { requirements: '' } });
+    console.log('5');
 
     await Offer.updateMany({}, { $unset: { isActive: '' } });
+    console.log('6');
     await Offer.updateMany({}, { $unset: { scopes: '' } });
+    console.log('7');
 
     await Booking.updateMany({}, { $unset: { eventId: '' } });
+    console.log('8');
 
     await Promise.all(a.map(async (el) => {
       el.creationDate = moment(el.creationDate).format('DD-MM-YYYY');
       await Booking.replaceOne({ _id: el._id }, el);
     }));
+    console.log('9');
 
     await User.updateMany({}, { $unset: { subscription: '' }});
+    console.log('10');
     await Place.updateMany({}, { $unset: { access: '' }});
+
+    return res.send('ok');
   });
 }
