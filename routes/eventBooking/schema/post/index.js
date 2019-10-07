191b1f94bd5b1d6428f1dd6b400e15596463a53a
Kamil <ase495@gmail.com>
2019-10-07 21:22:12 +0200

    Fix strict

--- routes/eventBooking/schema/post/index.js
+++ routes/eventBooking/schema/post/index.js
@@ -3,7 +3,7 @@ const Joi = require('@hapi/joi');
 const schema = Joi.object().keys({
   eventId: Joi.string().strict().required(),
   bookings: Joi.array().items(Joi.object().keys({
-    placeId: Joi.number().strict().required(),
+    placeId: Joi.number().integer().required(),
     intervalId: Joi.string().strict().required(),
     date: Joi.date().required(),
     offerIds: Joi.array().items(Joi.number().integer().strict()),
