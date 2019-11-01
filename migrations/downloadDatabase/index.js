dc3b69441c11f6e55eda1f7e16407bce0c5b3675
Kamil <ase495@gmail.com>
2019-11-01 19:57:57 +0100

    Update download db script

--- migrations/downloadDatabase/index.js
+++ migrations/downloadDatabase/index.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
+const moment = require('moment');
 
 const downloadDatabase = async (entities) => {
+  const dirname = moment().format('YYYY-MM-DD');
+  const directory = `migrations/db/${dirname}`;
   for (entity of Object.keys(entities)) {
     const data = await entities[entity].find({}).toArray();
-    fs.writeFile(`migrations/db/${entity}`, JSON.stringify(data), (err) => {
+    if (!fs.existsSync(directory)){
+      fs.mkdirSync(directory);
+    }
+    fs.writeFile(`${directory}/${entity}`, JSON.stringify(data), (err) => {
       if (err) console.log(err);
       console.log(`${entity} downloaded`);
     });
